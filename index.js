df2ca958d10382d8a9767ce6891b54117f7b45f4
CherrySudderth <emilysudderth@computingforall.org>
2021-08-12 14:15:17 -0700

    mergingjs

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log('Hello World!');
+var firstName = "Cherry";
+var lastName = "Sudderth";
+const age = 20; 
+
+firstName = "Berry";
+lastName = "Berryland";
+
+
+console.log('Hello ' + firstName + ' ' + lastName + '!' + ' You are ' + age + ' years old!'   );
