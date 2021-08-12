5bc74c46a8b82aa798fc12f7d520122fed88262e
M-A-Justice <michae1jus7ice@gmail.com>
2021-08-12 17:13:56 -0400

    completed variable workshop

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello World!');
+// var, let, or const indicates a declaration
+// when you see '=' it indicates an assignment
+var firstName = "Michael";
+let lastName = "Justice";
+// You cannot reassign constant variables!
+const age = 29;
+// variable reassignments
+firstName = "My Dude";
+lastName = "Potates";
+
+console.log('Hello ' + firstName + ' ' + lastName + '!' + ' You are ' + age + ' years old!');
